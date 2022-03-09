1c22c4b93d3928daf90d519c4d232a29329db3ff
Spyridon Peppas <sipmarch7@hotmail.com>
2022-03-09 20:18:57 +0200

    backend/ env var name change

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 exports.proxyMiddleware = createProxyMiddleware(
     {
-        target: process.env.PROXY_API,
+        target: process.env.PROXY_IP,
         changeOrigin: true,
         pathRewrite: {
             [`^/proxy`]: '',
