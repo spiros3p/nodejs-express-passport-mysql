003d1e8174874965a973ec76c032552652127ec2
sipmarch7 <sipmarch7@hotmail.com>
2022-03-11 22:25:57 +0200

    default proxy address to http://localhost - to have no error on empty .env input

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 exports.proxyMiddleware = createProxyMiddleware(
     {
-        target: process.env.PROXY_IP,
+        target: process.env.PROXY_IP || 'http://localhost',
         changeOrigin: true,
         pathRewrite: {
             [`^/proxy`]: '',
