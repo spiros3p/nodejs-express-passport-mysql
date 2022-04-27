0ee9627673c15d561f314a2944a3e2b55bef49f3
Spyridon Peppas <speppas@iit.demokritos.gr>
2022-04-27 17:17:44 +0300

    backend/ added change response origin to proxied requests

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
 const checkAuthentication = require("../passport/checkAuthentication");
 
@@ -11,5 +11,12 @@ exports.proxyMiddleware = createProxyMiddleware(filter, {
     changeOrigin: true,
     pathRewrite: {
         [`^/proxy`]: '',
-    }
+    },
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        // manipulate response from outside server and return to the client
+        const response = responseBuffer.toString('utf8'); // convert buffer to string
+        res.setHeader('access-control-allow-origin', req.headers.origin); //change the res.origin received from the API server to the one received-intented to the ANGULAR app
+        return response;
+    })
 })
